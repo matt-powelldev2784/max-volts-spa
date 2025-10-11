9c3381049e5b95e292efe13067c3017b22ac93ac
Matthew Powell <matt.powell2784@gmail.com>
2025-10-11 09:04:49 +0100

    feat: setup supabase auth

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,29 @@ const Home = () => {
     if (data) setClients(data);
   };
 
+  const login = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: { redirectTo: `${window.location.origin}/about` },
+    });
+  };
+
+  const logOut = async () => {
+    await supabase.auth.signOut();
+  };
+
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold text-blue-600">Max Volts - DB Test</h1>
+      <div className="flex gap-8">
+        <button className="bg-blue-500 text-white px-8 py-2" onClick={login}>
+          Login
+        </button>
+
+        <button className="bg-blue-500 text-white px-8 py-2" onClick={logOut}>
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
