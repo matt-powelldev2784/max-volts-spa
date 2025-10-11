86028a384d0679e6f85d8334cc0b664cc5655047
Matthew Powell <matt.powell2784@gmail.com>
2025-10-11 12:12:31 +0100

    refactor: move login and protected component to components folder

---
+++ src/components/auth/login.tsx
@@ -0,0 +1,36 @@
+import { supabase } from '../../lib/supabase';
+import useAuth from '../../lib/useAuth';
+
+const Login = () => {
+  const { user } = useAuth();
+
+  const login = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: { redirectTo: `${window.location.origin}/protected` },
+    });
+  };
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        {user ? user.email : 'Not Logged In'}
+      </h1>
+      <div className="flex gap-8">
+        <button className="bg-blue-500 text-white px-8 py-2" onClick={login}>
+          Login
+        </button>
+
+        <button className="bg-blue-500 text-white px-8 py-2" onClick={logout}>
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/protected/protected.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/supabase';
+import type { Tables } from '../../types/database.types';
+
+type Client = Tables<'client'>;
+
+const Protected = () => {
+  const [clients, setClients] = useState<Client[]>([]);
+  console.log('clients', clients);
+
+  useEffect(() => {
+    getClients();
+  }, []);
+
+  const getClients = async () => {
+    const { data } = await supabase.from('client').select();
+    if (data) setClients(data);
+  };
+
+  return (
+    <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">
+      Protected
+    </h1>
+  );
+};
+
+export default Protected;
