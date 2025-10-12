8d79f2e3cf3f466ce34aa95d51c7e267e72c99fc
Matthew Powell <matt.powell2784@gmail.com>
2025-10-12 19:37:04 +0100

    feat: add client and product skelton components

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,11 @@ import Login from '@/components/auth/login';
 import { NavigationBar } from '@/components/navigation/navigation';
 import ViewQuotes from '@/components/viewQuotes/viewQuotes';
 import AddQuote from '@/components/addQuote/addQuote';
-import SignOut from '@/components/auth/signOut';
+import SignOut from '@/components/auth/signout';
+import ViewClients from './components/viewClients/viewClients';
+import AddClient from './components/addClient/addClient';
+import ViewProducts from './components/viewProducts/viewProducts';
+import AddProduct from './components/addProduct/addProduct';
 
 const ProtectedLayouts = () => {
   const { user, loading } = useAuth();
@@ -36,8 +40,15 @@ const App = () => {
 
         <Route element={<NavigationBar />}>
           <Route element={<ProtectedLayouts />}>
+            <Route path="/view-clients" element={<ViewClients />} />
+            <Route path="/add-client" element={<AddClient />} />
+
+            <Route path="/view-products" element={<ViewProducts />} />
+            <Route path="/add-product" element={<AddProduct />} />
+
             <Route path="/view-quotes" element={<ViewQuotes />} />
             <Route path="/add-quote" element={<AddQuote />} />
+
             <Route path="/auth/signout" element={<SignOut />} />
           </Route>
         </Route>
