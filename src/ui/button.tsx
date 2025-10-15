57cb47da0580dadde2cf5e3166b4ca69c004eb83
Matthew Powell <matt.powell2784@gmail.com>
2025-10-15 21:49:54 +0100

    feat; add view clients table

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -17,6 +17,8 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground',
         ghost: 'text-foreground bg-neutral-200',
         link: 'text-primary underline-offset-4',
+        iconPrimary: 'bg-neutral-400 text-white',
+        iconGhost: 'bg-mv-green text-white',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
