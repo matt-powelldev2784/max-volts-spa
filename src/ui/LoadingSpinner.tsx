9909c6cd57a42a7f92d39f72387653ce00096625
Matthew Powell <matt.powell2784@gmail.com>
2025-10-16 18:07:55 +0100

    feat: move loading spinner to ui library

---
+++ src/ui/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import { Loader2 } from 'lucide-react';
+
+const LoadingSpinner = () => {
+  return <Loader2 className="animate-spin h-6 w-6 mx-auto mt-4 text-mv-orange" />;
+};
+
+export default LoadingSpinner;
