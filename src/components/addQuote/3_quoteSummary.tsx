27b31fba965d33d805ca5c9ed80d07d70a54007e
Matthew Powell <matt.powell2784@gmail.com>
2025-10-20 20:40:42 +0100

    feat: remove quote products props from quute summary

--- src/components/addQuote/3_quoteSummary.tsx
+++ src/components/addQuote/3_quoteSummary.tsx
@@ -1,13 +1,10 @@
-import type { QuoteProduct } from '@/types/dbTypes';
-
 type QuoteSummaryProps = {
   quoteId: number;
   clientId: number;
-  quoteProducts: QuoteProduct[];
 };
 
-const QuoteSummary = ({ quoteId, clientId, quoteProducts }: QuoteSummaryProps) => {
-  console.log('quoteId, clientId, quoteProducts', quoteId, clientId, quoteProducts);
+const QuoteSummary = ({ quoteId, clientId }: QuoteSummaryProps) => {
+  console.log('quoteId, clientId, quoteProducts', quoteId, clientId);
 
   return <div className="w-full text-center p-2">Quote Summary Component</div>;
 };
