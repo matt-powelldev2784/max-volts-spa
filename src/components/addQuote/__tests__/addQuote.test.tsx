b665260257defceaa3dba01fcbeb0d5f0712da42
Matthew Powell <matt.powell2784@gmail.com>
2025-11-09 10:31:52 +0000

    test: add test to check backwards navigation in add quote

--- src/components/addQuote/__tests__/addQuote.test.tsx
+++ src/components/addQuote/__tests__/addQuote.test.tsx
@@ -31,7 +31,6 @@ describe('addQuote', () => {
     // click the select client dropdown
     // fireEvent used instead of userEvent due to issues with select component
     const selectClientDropdownMenu = await screen.findByRole('combobox');
-
     fireEvent.click(selectClientDropdownMenu);
 
     // select a client from the dropdown and check that it is selected
@@ -48,4 +47,77 @@ describe('addQuote', () => {
     await user.click(nextButton);
     expect(await screen.findByText(/No products added./i)).toBeInTheDocument();
   });
-});
+
+  test('user should be able to select a client, navigate to add products step and navigate back to add client step', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <TestProviders>
+        <AddQuote />
+      </TestProviders>
+    );
+
+    // select a client
+    // fireEvent used instead of userEvent due to issues with select component
+    const selectClientDropdownMenu = await screen.findByRole('combobox');
+    fireEvent.click(selectClientDropdownMenu);
+
+    // select a client from the dropdown and check that it is selected
+    // fireEvent used instead of userEvent due to issues with select component
+    const testClientOption = await screen.findByRole('option', { name: /Test Client Test Company/i });
+    fireEvent.click(testClientOption);
+    waitFor(() => {
+      expect(selectClientDropdownMenu).toHaveTextContent(/Test Client Test Company/i);
+    });
+
+    // click the next button and check that the add products step is rendered
+    const nextButton = screen.getByRole('button', { name: /Next Step/i });
+    await user.click(nextButton);
+    expect(await screen.findByText(/No products added./i)).toBeInTheDocument();
+
+    // click the back button and check that the add client step is rendered
+    const backButton = screen.getByRole('button', { name: /Back/i });
+    await user.click(backButton);
+    expect(await screen.findByText(/Select a client/i)).toBeInTheDocument();
+  });
+
+  test('user should be able to navigate to back and the client should still be selected', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <TestProviders>
+        <AddQuote />
+      </TestProviders>
+    );
+
+    // select a client
+    // fireEvent used instead of userEvent due to issues with select component
+    const selectClientDropdownMenu = screen.getByRole('combobox');
+    fireEvent.click(selectClientDropdownMenu);
+
+    // select a client from the dropdown and check that it is selected
+    // fireEvent used instead of userEvent due to issues with select component
+    const testClientOption = await screen.findByRole('option', { name: /Test Client Test Company/i });
+    fireEvent.click(testClientOption);
+    waitFor(() => {
+      expect(selectClientDropdownMenu).toHaveTextContent(/Test Client Test Company/i);
+    });
+
+    // click the next button and check that the add products step is rendered
+    const nextButton = screen.getByRole('button', { name: /Next Step/i });
+    await user.click(nextButton);
+    expect(await screen.findByText(/No products added./i)).toBeInTheDocument();
+
+    // click the back button and check that the add client step is rendered
+    const backButton = screen.getByRole('button', { name: /Back/i });
+    await user.click(backButton);
+    expect(await screen.findByText(/Select a client/i)).toBeInTheDocument();
+
+    screen.debug(document.body, 10000);
+
+    const selectClientDropdownMenuAfterNavigate = screen.getByRole('combobox');
+    expect(selectClientDropdownMenuAfterNavigate).toHaveTextContent(/Test Client Test Company/i);
+  });
+}); 
+
+  
