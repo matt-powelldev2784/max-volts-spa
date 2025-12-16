188a4bf2f49c50e3055c6625b361a7c0218463ab
Matthew Powell <matt.powell2784@gmail.com>
2025-12-16 20:18:52 +0000

    feat: add hero section

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -4,11 +4,16 @@ import instagramLogo from '@/assets/insta.svg';
 import emailLogo from '@/assets/email_circle.svg';
 import telLogo from '@/assets/tel_circle.svg';
 import maxVoltsLogo from '@/assets/max_volts_logo.svg';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import { slideImages } from './slideData';
+import { Button } from '@/ui/button';
 
 const Homepage = () => {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   );
 };
@@ -48,3 +53,43 @@ export const NavBar = () => {
     </header>
   );
 };
+
+export const Hero = () => (
+  <section className="relative overflow-hidden bg-darkBlack">
+    <Fade arrows={false} duration={5000} transitionDuration={700} pauseOnHover={false}>
+      {slideImages.map(({ key, image, alt, title1, title2, title3, text }) => (
+        <div key={key} className="w-full h-[215px] md:h-[380px] lg:h-[620px] relative overflow-hidden">
+          {/* Overlay */}
+          <div className="absolute w-full h-full bg-black/60 z-10"></div>
+
+          {/* Background Image */}
+          <img
+            src={typeof image === 'string' ? image : (image as string)}
+            alt={alt}
+            className="object-cover w-full h-full absolute inset-0 z-0"
+          />
+
+          {/* Text Overlay */}
+          <div className="absolute top-0 flex flex-col items-center justify-center lg:items-start lg:left-32 lg:max-w-[750px] h-full w-full z-20 gap-4">
+            <p className="text-center max-w-[280px] md:max-w-[750px] lg:max-w-[680px] lg:text-left font-bold text-2xl md:text-3xl lg:text-5xl text-white mx-2 md:mx-16 lg:mx-0 rounded-xl mb-2">
+              {title1.toUpperCase()}
+              <span className="text-mv-orange">{' ' + title2.toUpperCase() + ' '}</span>
+              <span>{title3.toUpperCase()}</span>
+            </p>
+
+            <p className="hidden text-justify text-white text-sm mx-6 rounded-xl py-2 mb-4 md:mx-16 md:block md:text-base md:text-center lg:text-xl lg:text-left lg:mx-0 ">
+              {text}
+            </p>
+
+            {/* <a
+              href="#contact"
+              className="text-white bg-mv-orange flex flex-col items-center justify-center w-[265px] md:w-[300px] sm:max-h-[40px] max-h-[45px] mt-4 md:mt-0 rounded-xl font-bold"
+            > */}
+            <Button className="py-4 md:py-6 px-6 md:px-8 rounded-sm font-bold">GET A FREE QUOTE TODAY</Button>
+            {/* </a> */}
+          </div>
+        </div>
+      ))}
+    </Fade>
+  </section>
+);
